340dd4a9a588cd03e8a72bc3f5b7ccbdb1e17f80
fudzya <fudzya@gmail.com>
2019-05-09 14:29:36 +0300

    добавлены барьеры

--- src/lesson5/Car.java
+++ src/lesson5/Car.java
@@ -1,10 +1,13 @@
 package lesson5;
 
+import java.util.concurrent.CyclicBarrier;
+
 /**
  * @author liva
  */
 
 public class Car implements Runnable {
+
 	private static int CARS_COUNT;
 
 	static {
@@ -14,6 +17,8 @@ public class Car implements Runnable {
 	private Race   race;
 	private int    speed;
 	private String name;
+	private CyclicBarrier barrierWait;
+	private CyclicBarrier barrierGo;
 
 	public String getName() {
 		return name;
@@ -23,19 +28,23 @@ public class Car implements Runnable {
 		return speed;
 	}
 
-	public Car(Race race, int speed) {
+	public Car(Race race, int speed, CyclicBarrier barrierWait, CyclicBarrier barrierGo) {
 		this.race = race;
 		this.speed = speed;
 		CARS_COUNT++;
 		this.name = "Участник #" + CARS_COUNT;
+		this.barrierWait = barrierWait;
+		this.barrierGo = barrierGo;
 	}
 
 	@Override
 	public void run() {
 		try {
 			System.out.println(this.name + " готовится");
+			barrierWait.await();
 			Thread.sleep(500 + (int) (Math.random() * 800));
 			System.out.println(this.name + " готов");
+			barrierGo.await();
 		}
 		catch (Exception e) {
 			e.printStackTrace();
--- src/lesson5/MainClass.java
+++ src/lesson5/MainClass.java
@@ -1,23 +1,31 @@
 package lesson5;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 /**
  * @author liva
  */
 
 public class MainClass {
+
 	public static final int CARS_COUNT = 4;
 
 	public static void main(String[] args) {
+
+		CyclicBarrier barrierWait = new CyclicBarrier(CARS_COUNT);
+		CyclicBarrier barrierGo = new CyclicBarrier(CARS_COUNT, () ->
+				System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!"));
+
 		System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
 		Race race = new Race(new Road(60), new Tunnel(), new Road(40));
 		Car[] cars = new Car[CARS_COUNT];
 		for (int i = 0; i < cars.length; i++) {
-			cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+			cars[i] = new Car(race, 20 + (int) (Math.random() * 10), barrierWait, barrierGo);
 		}
 		for (int i = 0; i < cars.length; i++) {
 			new Thread(cars[i]).start();
 		}
-		System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-		System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+		//	System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
 	}
 }
