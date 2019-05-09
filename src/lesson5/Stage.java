d0b67a567f5e2a574931da8129956c0b3a17eae3
fudzya <fudzya@gmail.com>
2019-05-09 11:13:46 +0300

    Добавлен исходный код урока 5

---
+++ src/lesson5/Stage.java
@@ -0,0 +1,16 @@
+package lesson5;
+
+/**
+ * @author liva
+ */
+
+public abstract class Stage {
+	protected int    length;
+	protected String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public abstract void go(Car c);
+}
